74f0971023eba17595b59c4c39e5df33e2f8ecbc
lyt-s <1814666859@qq.com>
2023-12-02 17:49:01 +0800

    modified:   .gitignore 	modified:   CMakeLists.txt 	deleted:    UinquePtr.hpp 	new file:   UniquePtr.hpp 	new file:   test_uniqueptr.cpp

---
+++ test_uniqueptr.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+
+#include "UniquePtr.hpp"
+
+struct MyClass {
+  int a, b, c;
+};
+
+struct Animal {
+  virtual void speak() = 0;
+  virtual ~Animal() = default;
+};
+
+struct Dog : Animal {
+  int age;
+
+  Dog(int age_) : age(age_) {}
+
+  virtual void speak() { printf("Bark! I'm %d Year Old!\n", age); }
+};
+
+struct Cat : Animal {
+  int &age;
+
+  Cat(int &age_) : age(age_) {}
+
+  virtual void speak() { printf("Meow! I'm %d Year Old!\n", age); }
+};
+
+int main() {
+  std::vector<UniquePtr<Animal>> zoo;
+  int age = 3;
+  zoo.push_back(makeUnique<Cat>(age));
+  zoo.push_back(makeUnique<Dog>(age));
+  for (auto const &a : zoo) {
+    a->speak();
+  }
+  age++;
+  for (auto const &a : zoo) {
+    a->speak();
+  }
+  return 0;
+}
