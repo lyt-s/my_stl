c735f90df0c7c5188e5668f094b4764c6450e3a7
lyt-s <1814666859@qq.com>
2024-03-30 21:25:28 +0800

    modified:   design/test2.cpp 	modified:   design/test3.cpp 	new file:   design/test4.cpp 	new file:   os/io_uring.cpp 	modified:   s.md

--- design/test2.cpp
+++ design/test2.cpp
@@ -5,35 +5,13 @@
 using namespace std;
 
 int main() {
-  int n, x;
-  cin >> n >> x;
+  int n, x, y;
+  cin >> n >> x >> y;
 
-  vector<int> fans(n);
-  for (int i = 0; i < n; ++i) {
-    cin >> fans[i];
-  }
-  sort(fans.rbegin(), fans.rend());
+  int word = 0, listen = 0, read = 0;
 
-  int sum = 0, accounts = 0;
-  bool usedExtraPost = false;
+  word = (n + x - y) / 3;
 
-  for (int i = 0; i < n; ++i) {
-    if (!usedExtraPost && sum + fans[i] >= x) {
-      sum += fans[i];
-      accounts++;
-      break;
-    }
-    sum += fans[i] / 2;
-    accounts++;
-    if (sum >= x) {
-      break;
-    }
-  }
-  if (sum < x) {
-    cout << -1 << endl;
-  } else {
-    cout << accounts << endl;
-  }
-
+  cout << word - x << " " << word + y << " " << word;
   return 0;
 }
--- design/test3.cpp
+++ design/test3.cpp
@@ -1,15 +1,31 @@
-#include <iomanip>
+#include <algorithm>
 #include <iostream>
-
+#include <vector>
 using namespace std;
 
+vector<int> findFValues(const vector<int>& nums) {
+  vector<int> fValues;
+  int maxElement = *max_element(nums.begin(), nums.end());
+  for (int i = 0; i < nums.size(); ++i) {
+    fValues.push_back(max(maxElement, nums[i] * 2));
+  }
+  return fValues;
+}
+
 int main() {
-  double n;
+  int n;
   cin >> n;
+  vector<int> nums(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> nums[i];
+  }
 
-  double p = (2.0 / n) * (1 / (n - 1));
-
-  cout << fixed << std::setprecision(10) << p;
-
+  vector<int> fValues = findFValues(nums);
+  for (int i = 0; i < n; ++i) {
+    cout << fValues[i];
+    if (i < n - 1) {
+      cout << " ";
+    }
+  }
   return 0;
 }
