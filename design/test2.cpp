d93d39820b93004b5ba9eb1cc3ea1f0b7ace5ae0
lyt-s <1814666859@qq.com>
2024-05-02 22:40:06 +0800

    modified:   design/test2.cpp 	new file:   design/thread_sanitizer.cpp 	new file:   os/hanoi/Makefile 	new file:   os/hanoi/README.md 	new file:   os/hanoi/hanoi-main.c 	new file:   os/hanoi/hanoi-nr 	new file:   os/hanoi/hanoi-nr.c 	new file:   os/hanoi/hanoi-r 	new file:   os/hanoi/hanoi-r.c 	new file:   os/hello.cpp

--- design/test2.cpp
+++ design/test2.cpp
@@ -1,66 +1,20 @@
-#include <condition_variable>
-#include <cstdint>
-#include <functional>
-#include <iostream>
-#include <mutex>
-#include <queue>
-#include <thread>
-#include <utility>
-#include <vector>
-
-class ThreadPool {
- private:
-  bool m_stop = false;
-  std::mutex m_mutex;
-  std::condition_variable m_condition;
-  std::vector<std::thread> m_threads;
-  std::queue<std::function<void()>> m_taskQueue;
-
+#include "stdio.h"
+class A {
  public:
-  ThreadPool(int numThreads) {
-    for (int i = 0; i < numThreads; i++) {
-      m_threads.push_back(std::thread([this]() {
-        while (1) {
-          std::unique_lock<std::mutex> lock(m_mutex);
-          m_condition.wait(lock,
-                           [this]() { return m_stop || !m_taskQueue.empty(); });
-          if (m_stop && m_taskQueue.empty()) {
-            return;
-          }
-          auto task = m_taskQueue.front();
-          m_taskQueue.pop();
-          lock.unlock();
-          task();
-        }
-      }));
-    }
-  }
+  void f1() { printf("A::f1\n"); }
 
-  ~ThreadPool() {
-    {
-      std::unique_lock<std::mutex> lock(m_mutex);
-      m_stop = true;
-    }
-    m_condition.notify_all();
-    for (auto &i : m_threads) {
-      i.join();
-    }
-  }
-
-  template <class F, class... Args>
-  void enqueue(F &&f, Args &&...args) {
-    auto task = std::bind(std::forward<F>(f), std::forward<Args>(args)...);
-    {
-      std::unique_lock<std::mutex> lock(m_mutex);
-      m_taskQueue.push(task);
-    }
-    m_condition.notify_one();
-  }
+  virtual void f2() { printf("A::f2\n"); }
 };
+class B {
+ public:
+  virtual void f1() { printf("B::f1\n"); }
 
-void fun(int num) { std::cout << num << std::endl; }
-
+  virtual void f2() { printf("B::f2\n"); }
+};
 int main() {
-  ThreadPool pool(3);
-  pool.enqueue(fun, 9);
+  B b;
+  A *p = (A *)&b;
+  p->f1();
+  p->f2();
+  return 0;
 }
