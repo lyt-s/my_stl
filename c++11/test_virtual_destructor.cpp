529cec03fe2d9def2277edb25fe185d4adaced82
lyt-s <1814666859@qq.com>
2024-05-07 00:19:45 +0800

    modified:   c++11/test_virtual_destructor.cpp

--- c++11/test_virtual_destructor.cpp
+++ c++11/test_virtual_destructor.cpp
@@ -12,8 +12,30 @@ class Derived : public Base {
   }
 };
 
+class CrazyInt {
+  int v;
+  std::string m_name = "lyt";
+  int m_old = 18;
+
+ public:
+  CrazyInt(int i) : v(i) {}
+  operator int() const {
+    std::cout << m_name << " 年龄：" << m_old << std::endl;
+    if (v == 0) return false;
+    return true;
+  }  // conversion from CrazyInt to int
+};
+
+void print_class(int a) { std::cout << a << std::endl; }
+
 int main() {
-  Base* base_ptr = new Derived();
-  delete base_ptr;  // 首先调用Derived的析构函数，然后调用Base的析构函数
-  return 0;
+  CrazyInt b = 55;
+  print_class(999);  // prints 999
+  print_class(b);    // prints 55
 }
+
+// int main() {
+//   Base* base_ptr = new Derived();
+//   delete base_ptr;  // 首先调用Derived的析构函数，然后调用Base的析构函数
+//   return 0;
+// }
